d63d5d2be8bbab73204ba330b5a1b4960f1deb72
Olivia Cameron <oliviacameron@Olivias-MacBook-Air.local>
2020-11-23 23:24:24 -0500

    add: Main section

---
+++ src/components/Main.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import { api } from "../utils/Api.js";
+import Cards from "./Cards.js";
+import ImagePopup from "./ImagePopup.js";
+
+function Main(props) {
+  const [userName, setUserName] = useState("");
+  const [userDescription, setUserDescription] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    api
+      .getUserData()
+      .then(([userData, initialCards]) => {
+        setUserName(userData.name);
+        setUserDescription(userData.description);
+        setUserAvatar(userData.avatar);
+        console.log(initialCards);
+        return initialCards;
+      })
+      .then((res) => {
+        setCards(
+          res.map((card) => ({
+            link: card.link,
+            name: card.name,
+            _id: card._id,
+            likes: card.likes.length,
+          }))
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <main className="content">
+      <section className="profile section">
+        <div className="profile__container">
+          <div className="profile__avatar-container">
+            <img
+              src={userAvatar}
+              alt="A profile of Jacques Cousteau"
+              className="profile__avatar"
+            />
+            <button
+              aria-label="Edit-Avatar"
+              type="button"
+              className="profile__avatar-overlay"
+              onClick={props.onEditAvatar}
+            ></button>
+          </div>
+
+          <div className="profile__info">
+            <h1 className="profile__heading">{userName}</h1>
+            <button
+              aria-label="Edit"
+              type="button"
+              className="profile__editBtn"
+              onClick={props.onEditProfile}
+            ></button>
+            <p className="profile__occupation">{userDescription}</p>
+          </div>
+          <button
+            aria-label="Add"
+            type="button"
+            className="profile__addBtn"
+            onClick={props.onAddPlace}
+          ></button>
+        </div>
+      </section>
+
+      <section className="elements section">
+        <ul className="elements__container">
+          {cards.map((card) => {
+            return (
+              <Cards
+                key={card._id}
+                src={card.link}
+                name={card.name}
+                likes={card.likes}
+                card={card}
+                onCardClick={props.onCardClick}
+              />
+            );
+          })}
+        </ul>
+      </section>
+
+      <ImagePopup
+        card={props.selectedCard}
+        isOpen={props.isImageOpen}
+        onClose={props.onClose}
+      />
+    </main>
+  );
+}
+
+export default Main;
