f4928685e56a439bedb0a80203c2aff1b3a5b4ed
Olivia Cameron <olivia.cameronj15@gmail.com>
2020-11-24 10:27:40 -0500

    hotfix: use template literals

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -9,21 +9,21 @@ class Api {
     }
 
     getInitialCards() {
-        return fetch(this._baseUrl + '/cards', {
+        return fetch(`${this._baseUrl}/cards`, {
             headers: this._headers
         })
-        .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+        .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`))
     }
 
     getUserInfo() {
-        return fetch(this._baseUrl + '/users/me', {
+        return fetch(`${this._baseUrl}/users/me`, {
             headers: this._headers
         })
-        .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+        .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`))
     }
 
     addCard({ link, name }) {
-        return fetch(this._baseUrl + '/cards', {
+        return fetch(`${this._baseUrl}/cards`, {
             headers: this._headers,
             method: "POST",
             body: JSON.stringify({
@@ -31,19 +31,19 @@ class Api {
                 link
             })
         })
-        .then((res) => res.ok ? res.json() : Promise.reject('Error' + res.statusText))
+        .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`))
     }
     
     deleteCard(cardId) {
-        return fetch(this._baseUrl + '/cards/' + cardId, {
+        return fetch(`${this._baseUrl}/cards/${cardId}`, {
             headers: this._headers,
             method: "DELETE"
         })
-        .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+        .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`))
     }
 
     editUserInfo(data) {
-        return fetch(this._baseUrl + '/users/me', {
+        return fetch(`${this._baseUrl}/users/me`, {
             headers: this._headers,
             method: "PATCH",
             body: JSON.stringify({
@@ -51,31 +51,31 @@ class Api {
                 about: data.title
             })
         })
-        .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+        .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`))
     }
 
     changeLikeStatus(cardId, boolean) {
         if (boolean) {
-            return fetch(this._baseUrl + '/cards/likes/' + cardId, {
+            return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
             headers: this._headers,
             method: "PUT"}) 
-            .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+            .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`))
         } else {
-            return fetch(this._baseUrl + '/cards/likes/' + cardId, {
+            return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
             headers: this._headers,
             method: "DELETE"})
-            .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+            .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`))
         }
         }
     setUserAvatar(avatar){
-        return fetch(this._baseUrl + '/users/me/avatar', {
+        return fetch(`${this._baseUrl}/users/me/avatar`, {
             headers: this._headers,
             method: "PATCH",
             body: JSON.stringify({
                 avatar
             })
         })
-        .then((res) => {res.ok ? res.json() : Promise.reject('Error!' + res.statusText)})
+        .then((res) => {res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`)})
     }
 }
 
