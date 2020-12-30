a7ec23a34c3f6c700a5236c37fe9ef064d3a64d3
Olivia Cameron <olivia.cameronj15@gmail.com>
2020-12-30 08:38:07 -0500

    refactor: api file naming

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import ImagePopup from "./ImagePopup.js";
 import EditProfilePopup from "./EditProfilePopup.js";
 import EditAvatarPopup from "./EditAvatarPopup.js";
 import AddPlacePopup from "./AddPlacePopup.js";
-import api from '../utils/Api.js';
+import api from '../utils/api.js';
 import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 
 function App() {
